7614d13a739fa35a2f3d67761ca4c778921a8da3
aravind-kondapu <109215886+aravind-kondapu@users.noreply.github.com>
2023-10-16 16:26:24 +0530

    updated database server

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose') // mongoose is used of the use of mongo db 
 const morgan = require('morgan') // login console
 const bodyParse = require('body-parser') // parse the request incoming bodies
 
-mongoose.connect('mongodb://localhost:27017/nxtrestro', {useNewUrlParser: true, useUnifiedTopology: true})
+// mongoose.connect('mongodb://localhost:27017/nxtrestro', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://surendrareddyk257:aravindsr257@cluster0.7rcugnx.mongodb.net', {useNewUrlParser: true, useUnifiedTopology: true})
+
 const db = mongoose.connection
 
 const usersRoute = require('./app/routes/usersRoute')
@@ -35,11 +37,12 @@ app.use(function(req, res, next) {
 });
 
 // to run with an IP address
-app.get('/', function(req, res){
-    let ipAddress = req.socket.remoteAddress;
-    console.log(ipAddress);
-    res.send(ipAddress);
-})
+// app.get('/', function(req, res){
+//     let ipAddress = req.socket.remoteAddress;
+//     console.log(ipAddress);
+//     res.send(ipAddress);
+// })
+
 const PORT = process.env.PORT || 9000
 app.listen(PORT, ()=>{
     console.log(`Server running at ${PORT}`)
